44646a5b2168dbd158fb68ebfaf63aff4c591ffc
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2018-01-22 16:40:55 -0500

    db config modularized and completed

---
+++ app/db/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const config = require('../config');
+const mongoose = require('mongoose').connect(config.dbURI);
+
+//
+
+mongoose.connection.on('error', error => {
+  console.log("MongoDB Error: ", error)
+});
+
+module.exports = {
+  Mongoose
+}
